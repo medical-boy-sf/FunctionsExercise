23ad95c102fb4ab359533335d337d35473fe4b54
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-02-17 13:29:08 +0200

    Characters in Range

---
+++ 03.characterInRange.js
@@ -0,0 +1,26 @@
+function solve(character1, character2) {
+    let message = '';
+    let min = (a, b) => {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+    };
+
+    let max = (a, b) => {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    };
+    let start = min(character1.charCodeAt(0), character2.charCodeAt(0));
+    let end = max(character1.charCodeAt(0), character2.charCodeAt(0));
+    for (let i = start + 1; i < end; i++) {
+        message += String.fromCharCode(i) + " ";
+    }
+
+    console.log(message);
+
+}
